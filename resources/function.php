497a9b94cb4a9b4d308a33232329d16942c72976
Samin Yeaser <saminyeaser1@gmail.com>
2020-01-07 15:27:34 +0600

    category page showing

--- resources/function.php
+++ resources/function.php
@@ -42,12 +42,12 @@ function get_products(){
 
     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320*150" alt="">
+                            <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
                             <div class="caption">
-                                <h4 class="pull-right">{$row['product_price']}</h4>
-                                <h4><a href="#">Third product</a></h4>
+                                <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+                                <h4><a href="product.html">{$row['product_title']}</a></h4>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                                <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                                <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to Cart</a>
 
                             </div>
 
@@ -59,6 +59,23 @@ DELI;
       echo $product;
 
     }
+}
+
+function get_category(){
+
+        global $connection;
+
+    $query = "SELECT * FROM catagories";
+    $send_query = mysqli_query($connection, $query);
+
+    if(!$send_query){
+        die("Conncetion Failed". mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_array($send_query)){
+        echo "<a href='category.php?id={$row["cat_id"]}' class='list-group-item'>{$row['cat_title']}</a>";
+    }
+
 }
 //getProduct
 
