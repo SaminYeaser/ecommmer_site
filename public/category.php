4a46dc7e6f93156fa433329eab6fe69ddf4442d6
Samin Yeaser <saminyeaser1@gmail.com>
2020-01-11 00:52:37 +0600

    functioning

--- public/category.php
+++ public/category.php
@@ -19,7 +19,7 @@
         <!-- Title -->
         <div class="row">
             <div class="col-lg-12">
-                <h3>Latest Features</h3>
+                <h3>Latest Products</h3>
             </div>
         </div>
         <!-- /.row -->
