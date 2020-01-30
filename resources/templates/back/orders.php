9f8fde8d18a1bce16cbf000ffa8f3872a35316a9
Samin Yeaser <saminyeaser1@gmail.com>
2020-01-31 00:00:34 +0600

    display function created

--- resources/templates/back/orders.php
+++ resources/templates/back/orders.php
@@ -33,17 +33,8 @@
       </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>21</td>
-            <td>Nikon 234</td>
+    <?php display(); ?>
 
-            <td><img src="http://placehold.it/62x62" alt=""></td>
-            <td>Cameras</td>
-            <td>456464</td>
-            <td>Jun 2039</td>
-           <td>Completed</td>
-        </tr>
-        
 
     </tbody>
 </table>
