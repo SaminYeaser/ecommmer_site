49a690c8d8ab32e15bf21deaad1f3aebd54fcbe3
Samin Yeaser <saminyeaser1@gmail.com>
2020-01-31 01:40:35 +0600

    delete order page made

--- resources/templates/back/delete_orders.php
+++ resources/templates/back/delete_orders.php
@@ -1,1 +1,17 @@
 <?php require_once ("../../../resources/config.php");?>
+
+<?php
+
+if(isset($_GET['id'])){
+
+    $delete_order = query("delete from orders where order_id=".escape_string($_GET['id']));
+    confirm($delete_order);
+
+    set_message("Your order have been deleted!");
+    redirect("http://localhost/ecom/public/admin/index.php?orders");
+
+}else{
+    redirect("http://localhost/ecom/public/admin/index.php?orders");
+}
+
+?>
