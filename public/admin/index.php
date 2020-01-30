4e9f6930964a348c35208db93dd0512beb0c5dfc
Samin Yeaser <saminyeaser1@gmail.com>
2020-01-31 01:50:35 +0600

    pain in the ass

--- public/admin/index.php
+++ public/admin/index.php
@@ -11,23 +11,7 @@ if(!isset($_SESSION['user_name'])){
 ?>
 
 
-        <div id="page-wrapper">
 
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Dashboard <small>Statistics Overview</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li class="active">
-                                <i class="fa fa-dashboard"></i> Dashboard
-                            </li>
-                        </ol>
-                    </div>
-                </div>
                 <!-- /.row -->
 
 
